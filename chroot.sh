1d0d1566f9cf77385c7735e2c2456cfad2a8ba5d
Luiz Schonarth <luizcschonarth@gmail.com>
2022-06-09 18:48:23 -0300

    Changed boot manager from GRUB to systemd-boot

--- chroot.sh
+++ chroot.sh
@@ -11,6 +11,8 @@ TIMEZONE=$timezone
 LANGUAGES=(en_US pt_BR)
 KEYMAP=$keymap
 
+ROOT_PART=$1
+
 echo '----------------------------'
 echo "HOSTNAME: ${HOSTNAME}" 
 echo "USER: ${USER}" 
@@ -43,7 +45,7 @@ systemctl enable bluetooth.service
 
 # Install Intel Microcode
 cpu_vendor=$(lscpu | grep Vendor | awk '{print $3}')
-if [[ $cpu_vendor = "GenuineIntel" ]]; then
+if [[ $cpu_vendor == "GenuineIntel" ]]; then
 	# ------- GenuineIntel processor detected -------
 	pacman -S intel-ucode
 fi
@@ -51,9 +53,59 @@ fi
 # Install graphical drivers
 zsh /scripts/drivers.sh
 
-# Generate GRUB configuration
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+# Install and configure bootloader
+bootctl install
+cat > /etc/pacman.d/hooks/100-systemd-boot.hook << EOF
+[Trigger]
+Type = Package
+Operation = Upgrade
+Target = systemd
+
+[Action]
+Description = Gracefully upgrading systemd-boot...
+When = PostTransaction
+Exec = /usr/bin/systemctl restart systemd-boot-update.service
+EOF
+
+cat > /etc/pacman.d/hooks/99-secureboot.hook << EOF
+[Trigger]
+Operation = Install
+Operation = Upgrade
+Type = Package
+Target = linux
+Target = systemd
+
+[Action]
+Description = Signing Kernel for SecureBoot
+When = PostTransaction
+Exec = /usr/bin/find /boot -type f ( -name vmlinuz-* -o -name systemd* ) -exec /usr/bin/sh -c 'if ! /usr/bin/sbverify --list {} 2>/dev/null | /usr/bin/grep -q "signature certificates"; then /usr/bin/sbsign --key db.key --cert db.crt --output "$1" "$1"; fi' _ {} ;
+Depends = sbsigntools
+Depends = findutils
+Depends = grep
+EOF
+
+PARTUUID=$(blkid -s PARTUUID -o value ${ROOT_PART})
+
+if pacman -Q linux; then
+	conf_path="/boot/loader/entries/arch.conf"
+
+	cat > $conf_path <<- EOF
+		title	Arch Linux
+		linux	/vmlinuz-linux
+		initrd	/initramfs-linux.img
+		options	root=PARTUUID=${PARTUUID} rw
+	EOF
+
+	if [[ $cpu_vendor == "GenuineIntel" ]]; then
+		sed -i -e '3i initrd	/intel-ucode.img' $conf_path
+	fi
+fi
+
+cat > /boot/loader/loader.conf << EOF
+default	arch.conf
+timeout	3
+editor	0
+EOF
 
 # Create wheel user
 useradd -mG wheel,storage,power,video,audio $USER
