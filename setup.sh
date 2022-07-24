37f502acb9901e665873a3b9796984b923127b2a
Luiz Schonarth <luizcschonarth@gmail.com>
2022-07-24 18:26:52 -0300

    setup: Add opts parsing

--- setup.sh
+++ setup.sh
@@ -1,39 +1,55 @@
-usage() {
-	echo "Lel"
+print_help() {
+	echo "Usage: $0 [OPTIONS]"
 }
 
-D_PACKAGES=false
+# Parse short and long opts
+TEMP=$(getopt --options vghd --longoptions verbose,gaming,aur,grub,help,dotfiles -- "$@")
 
-# Fetch opts
-while getopts "d" o; do
-	case $o in
-		"d") D_PACKAGES=true ;;
-		*)   usage ;;
+# Terminates program if getopt reports an error
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+eval set -- "$TEMP"
+
+VERBOSE=false
+GAMING=false
+AUR=false
+GRUB=false
+DOTFILES=false
+
+# Set variables
+while true; do
+	case "$1" in
+		-h | --help		) print_help; exit 0 ;;
+		-v | --verbose	) VERBOSE=true; shift ;;
+		--gaming		) GAMING=true; shift ;;
+		--aur		    ) AUR=true; shift ;;
+		-g | --grub	    ) GRUB=true; shift ;;
+		-d | --dotfiles ) DOTFILES=true; shift ;;
+		--				) shift ; break ;;
+		*				) break ;;
 	esac
 done
 
 . "./partitions.conf"
 
-# Variables from the partitions.conf file
-ROOT_PART=$root_partition
-SWAP_PART=$swap_partition
-HOME_PART=$home_partition
-VAR_PART=$var_partition
-EFI_PART=$efi_partition
+if [ $VERBOSE = true ]; then
+	# Variables from the partitions.conf file
+	echo '----------------------------'
+	echo "ROOT: ${root_partition}" 
+	echo "EFI: ${efi_partition}" 
+	echo "SWAP: ${swap_partition}" 
+	echo "HOME: ${home_partition}" 
+	echo "VAR: ${var_partition}" 
+	echo '----------------------------'
+fi
 
-echo '----------------------------'
-echo "ROOT: ${ROOT_PART}" 
-echo "EFI: ${EFI_PART}" 
-echo "SWAP: ${SWAP_PART}" 
-echo "HOME: ${HOME_PART}" 
-echo "VAR: ${VAR_PART}" 
-echo '----------------------------'
-
-mount $ROOT_PART /mnt
-[[ -n $SWAP_PART ]] && swapon $SWAP_PART
-[[ -n $HOME_PART ]] && mount --mkdir $HOME_PART /mnt/home
-[[ -n $VAR_PART ]] && mount --mkdir $VAR_PART /mnt/var
-[[ -n $EFI_PART ]] && mount --mkdir $EFI_PART /mnt/boot
+# Mount the partition especified in partitions.conf
+# TODO: maybe accept partitions as a opt
+mount $root_partition /mnt
+[[ -n $swap_partition ]] && swapon $swap_partition
+[[ -n $home_partition ]] && mount --mkdir $home_partition /mnt/home
+[[ -n $var_partition ]] && mount --mkdir $var_partition /mnt/var
+[[ -n $efi_partition ]] && mount --mkdir $efi_partition /mnt/boot
 
 timedatectl set-ntp true
 
@@ -45,24 +61,25 @@ essential_packages=(
 basic_packages=(
 	"man-db" "man-pages" "texinfo"
 	"networkmanager" 
-	"sudo"
+	"sudo" "zsh"
 )
 
-all_packages=(
+first_packages=(
 	${essential_packages[@]}
 	${basic_packages[@]}
 )
 
-pacstrap /mnt ${all_packages[@]}
+pacstrap /mnt ${first_packages[@]}
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 SCRIPT_DIR=/mnt/scripts
 mkdir -p $SCRIPT_DIR
-mv chroot.sh packages.sh drivers.sh xorg.sh user.conf $SCRIPT_DIR
-arch-chroot /mnt zsh /scripts/chroot.sh $ROOT_PART
+mv /arch-bootstrap $SCRIPT_DIR
 
-rm -r /mnt/scripts
+arch-chroot /mnt /bin/zsh -c "VERBOSE=${VERBOSE}; GAMING=${GAMING}; AUR=${AUR}; GRUB=${GRUB}; DOTFILES=${DOTFILES}" /scripts/chroot.sh $root_partition
+
+rm -r $SCRIPT_DIR
 
 umount -R /mnt
 reboot
