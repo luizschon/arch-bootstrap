87a0441c46bb17cf5a7cca9f3e49b3801f2e9f3b
Luiz Schonarth <luizcschonarth@gmail.com>
2022-07-24 23:34:52 -0300

    system: Rename xorg.sh and add input methods configurations

---
+++ system_config.sh
@@ -0,0 +1,32 @@
+#! /bin/zsh
+
+. "/scripts/system.conf"
+
+# Creates user directories
+xdg-user-dirs-update
+
+# Input methods configuration
+cat > /etc/X11/xorg.conf.d/00-keyboard.conf << EOF
+Section "InputClass"
+        Identifier "Keyboard"
+        MatchIsKeyboard "on"
+        Option "XkbLayout" "${kblayout}"
+        Option "XkbVariant" "${kbvariant}"
+EndSection
+EOF
+
+cat > /etc/X11/xorg.conf.d/30-touchpad.conf << EOF
+Section "InputClass"
+    Identifier "Touchpad"
+    Driver "libinput"
+    MatchIsTouchpad "on"
+    Option "NaturalScrolling" "true"
+    Option "Tapping" "on"
+EndSection
+EOF
+
+# Bluetooth configuration
+# Enables auto power-on after boot/resume
+sed -i 's/#\(AutoEnable=\).*/\1true/' /etc/bluetooth/main.conf
+
+exit
